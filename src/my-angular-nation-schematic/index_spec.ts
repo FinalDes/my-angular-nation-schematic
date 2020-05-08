e1056b3b57d95fea5963643c86f6130addb84c6d
Alex Alonso <alex4551@gmail.com>
2020-05-08 20:41:09 +0800

    fix unit test

--- src/my-angular-nation-schematic/index_spec.ts
+++ src/my-angular-nation-schematic/index_spec.ts
@@ -6,10 +6,10 @@ import * as path from 'path';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 
-describe('my-angular-nations-chematic', () => {
+describe('my-angular-nation-schematic', () => {
   it('works', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('my-angular-nations-chematic', {}, Tree.empty());
+    const tree = runner.runSchematic('my-angular-nation-schematic', {}, Tree.empty());
 
     expect(tree.files).toEqual([]);
   });

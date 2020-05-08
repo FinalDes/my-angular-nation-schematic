6b4aa53560c058de4f8b5f76c9965e13de0ff6f6
Alex Alonso <alex4551@gmail.com>
2020-05-08 18:52:01 +0800

    finish lesson 5

---
+++ src/create-structure/index.ts
@@ -0,0 +1,35 @@
+import { 
+  Rule,
+  SchematicContext,
+  Tree,
+  SchematicsException,
+  chain,
+  apply,
+  move,
+  template,
+  url,
+  branchAndMerge,
+  mergeWith 
+} from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function createStructure(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const angularConfig = tree.read('/angular.json');
+    if(!angularConfig){
+      throw new SchematicsException('You are not in an Angular Project. Good Bye!');
+    }
+    const source =  apply(url('./files'),[
+      template({
+        ...strings,
+        ..._options as object
+      } as any),
+      move(_options.path)
+    ])
+    return chain([branchAndMerge(chain([mergeWith(source)]))])(tree,_context);
+    // return tree;
+  };
+}
--- src/my-angular-nation-schematic/index.ts
+++ src/my-angular-nation-schematic/index.ts
@@ -1,13 +1,16 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, chain, schematic } from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function myAngularNationSchematic(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    _context.logger.info('Hey I am running your schemactics!');
-    const filepath = 'angular-nation.ts';
-    tree.create(filepath,'Angular Nation is the best platform to meet friend');
-    return tree;
-  };
+  return chain([
+    schematic('create-structure',_options),
+    (tree: Tree, _context: SchematicContext) => {
+      _context.logger.info('Hey I am running your schemactics!');
+      const filepath = 'readme.md';
+      tree.create(filepath,'Angular Nation is the best platform to meet friend');
+      return tree;
+    },
+  ]);
 }

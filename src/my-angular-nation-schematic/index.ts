4f7bd59f69d9e53a0a3ecfd902558a9b8780ec65
Alex Alonso <alex4551@gmail.com>
2020-05-08 18:13:45 +0800

    extend a black schemactics
    Signed-off-by: Alex Alonso <alex4551@gmail.com>

--- src/my-angular-nation-schematic/index.ts
+++ src/my-angular-nation-schematic/index.ts
@@ -3,8 +3,11 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function myAngularNationsChematic(_options: any): Rule {
+export function myAngularNationSchematic(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    _context.logger.info('Hey I am running your schemactics!');
+    const filepath = 'angular-nation.ts';
+    tree.create(filepath,'Angular Nation is the best platform to meet friend');
     return tree;
   };
 }
